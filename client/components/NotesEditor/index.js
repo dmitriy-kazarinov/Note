0863524e2e0ad4c0b6318821bd71fd0f5cad9af4
dmitry-dulin <dmitry.dulin@ainstainer.de>
2016-11-21 12:36:08 +0200

    Add form and refactoring editor

--- client/components/NotesEditor/index.js
+++ client/components/NotesEditor/index.js
@@ -5,18 +5,77 @@ import getStyles from './styles'
 
 class NotesEditor extends React.Component {
 
-  clearEdit(event) {
+  constructor (props) {
+    super(props)
+
+    this.clearEdit = this.clearEdit.bind(this)
+    this.toggleEdit = this.toggleEdit.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleTitle = this.handleTitle.bind(this)
+    this.handleTitle = this.handleTitle.bind(this)
+    this.handleText = this.handleText.bind(this)
+
+    this.state = {
+      isOpenEditor: false,
+      title: '',
+      text: ''
+    }
+  }
+
+  clearEdit (event) {
     event.preventDefault()
-    this.noteTitle.value = ''
+    this.setState({
+      title: '',
+      text: ''
+    })
+  }
+
+  toggleEdit (event) {
+    event.preventDefault()
+    this.setState({isOpenEditor: !this.state.isOpenEditor})
+  }
+
+  handleSubmit (event) {
+    event.preventDefault()
+    console.log(this.state)
+  }
+  handleTitle (event) {
+    this.setState({title: event.target.value})
+  }
+
+  handleText (event) {
+    this.setState({text: event.target.value})
   }
 
   render () {
     const styles = getStyles()
 
+    let editor
+    if (this.state.isOpenEditor) {
+      editor = (
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type='text'
+            placeholder='Write title'
+            value={this.state.title}
+            onChange={this.handleTitle}
+          />
+          <textarea
+            placeholder='Write text'
+            value={this.state.text}
+            onChange={this.handleText}
+          />
+          <button>Save</button>
+          <button onClick={this.clearEdit}>Clear</button>
+        </form>
+      )
+    }
     return (
       <div>
-        <input type='text' ref={title => this.noteTitle = title} />
-        <button onClick={this.clearEdit.bind(this)}>Clear</button>
+        <button onClick={this.toggleEdit}>{
+          this.state.isOpenEditor ? 'Close' : 'Open'
+        }</button>
+        {editor}
       </div>
     )
   }
